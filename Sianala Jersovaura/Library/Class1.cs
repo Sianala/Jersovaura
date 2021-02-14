7d8b720e0a804b804a027d71097c3451ab0f8208
A. Moiseev <malexey@mail.sianala.cf>
2021-02-14 17:08:16 +0700

    Jersovaura first update.

---
+++ Sianala Jersovaura/Library/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sianala.Jersovaura.Drive
+{
+    public class File
+    {
+        public static void Create(string Path)
+        {
+            System.IO.File.Create(Path);
+        }
+        public static void Create(string Path, string Content)
+        {
+            System.IO.File.WriteAllText(Path, Content);
+        }
+        public static void Rename(string Path, string NewName)
+        {
+            var File = new System.IO.FileInfo(Path);
+        }
+    }
+}
