7f6cb5205926bd47766298fdfb097a832ec142b6
Boris E. Moiseev <bmoiseev@dataeast.com>
2021-02-17 10:58:16 +0700

    Direcotry config.

--- Sianala Jersovaura/Library/Directory.cs
+++ Sianala Jersovaura/Library/Directory.cs
@@ -28,4 +28,44 @@ namespace Sianala.Jersovaura.Drive
         }
         
     }
+    public class FolderTime
+    {
+        public class Get
+        {
+            public static DateTime Creation(string Path)
+            {
+                return Directory.GetCreationTime(Path);
+            }
+            public static DateTime Write(string Path)
+            {
+                return Directory.GetLastWriteTime(Path);
+            }
+            public static DateTime Access(string Path)
+            {
+                return Directory.GetLastAccessTime(Path);
+            }
+        }
+        public class Set
+        {
+            public static void Creation(string Path, DateTime Time)
+            {
+                Directory.SetCreationTime(Path, Time);
+            }
+            public static void Write(string Path, DateTime Time)
+            {
+                Directory.SetLastWriteTime(Path, Time);
+            }
+            public static void Access(string Path, DateTime Time)
+            {
+                Directory.SetLastAccessTime(Path, Time);
+            }
+        }
+    }
+    public class FolderInfo
+    {
+        public static string FullPath(string Path)
+        {
+            return System.IO.Path.GetFullPath(Path);
+        }
+    }
 }
